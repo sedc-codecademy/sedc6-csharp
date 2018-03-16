8975272a1ad5c5ff0e36d584deab45b4c6e2e32c
Dzevat Ibraimi <Dzevat.Ibraimi@seavus.com>
2018-03-16 18:49:53 +0100

    added datastructures project

---
+++ src/Advanced01/DataStructures/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(1);
+            numbers.AddRange(new int[] { 1, 2, 3, 4 });
+            //Console.WriteLine(numbers.Capacity);
+            List<string> colors = new List<string>
+{"red","blue","black", "white" };
+            //Stack<> = new Stack<int>
+
+            PrintList<int>(numbers);
+            PrintList(colors);
+            Console.ReadKey();
+        }
+        private static void PrintList<T>(List<T> numbers)
+        {
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        //private static void PrintList(List<char> numbers)
+        //{
+        //    foreach (var number in numbers)
+        //    {
+        //        Console.WriteLine(number);
+        //    }
+        //}
+
+        //private static void PrintList(List<double> numbers)
+        //{
+        //    foreach (var number in numbers)
+        //    {
+        //        Console.WriteLine(number);
+        //    }
+        //}
+        //private static void PrintList(List<int> numbers)
+        //{
+        //    foreach (var number in numbers)
+        //    {
+        //        Console.WriteLine(number);
+        //    }
+        //}
+        //private static void PrintList(List<string> items)
+        //{
+        //    foreach (var item in items)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+        //}
+    }
+}
