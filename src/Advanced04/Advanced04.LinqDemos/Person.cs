a282f2297b26adfa133bbf7b98caedbd78057a00
Dzevat Ibraimi <Dzevat.Ibraimi@seavus.com>
2018-03-24 15:00:20 +0100

    ADDED .Min() .Max() methods

--- src/Advanced04/Advanced04.LinqDemos/Person.cs
+++ src/Advanced04/Advanced04.LinqDemos/Person.cs
@@ -2,12 +2,28 @@
 
 namespace Advanced04.LinqDemos
 {
-    public class Person
+    public class Person //: IComparable<Person>
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Age { get; set; }
+
+        //public int CompareTo(Person other)
+        //{
+        //    if (this.Age > other.Age)
+        //    {
+        //        return 1;
+        //    }
+        //    else if (this.Age == other.Age)
+        //    {
+        //        return 0;
+        //    }
+        //    else
+        //    {
+        //        return -1;
+        //    }
+        //}
 
         public override string ToString()
         {
--- src/Advanced04/Advanced04.LinqDemos/Program.cs
+++ src/Advanced04/Advanced04.LinqDemos/Program.cs
@@ -11,7 +11,7 @@ namespace Advanced04.LinqDemos
             IEnumerable<Person> people = new List<Person>
             {
                 new Person{Id=1,Age=12,FirstName="person1",LastName="mcPerson1" },
-                new Person{Id=2,Age=18,FirstName="person2",LastName="mcPerson2" },
+                new Person{Id=1,Age=18,FirstName="person2",LastName="mcPerson2" },
                 new Person{Id=3,Age=13,FirstName="person3",LastName="mcPerson3" },
                 new Person{Id=4,Age=28,FirstName="person4",LastName="mcPerson4" },
                 new Person{Id=5,Age=25,FirstName="person5",LastName="mcPerson5" }
@@ -55,6 +55,22 @@ namespace Advanced04.LinqDemos
             //    last.Reverse();
             //}
 
+            //.Single() .SingleOrDefault()
+            //var singlePerson = people.Single(p => p.Id == 1);
+            //var singleZeroElement = new List<int>().Single(n => n == 0);
+            //var singlePerson = people.Single(p => p.Id ==3);
+
+            //var singlePerson = people.SingleOrDefault(p => p.Id == 1);
+            //var singleZeroElement = new List<int>().SingleOrDefault(n => n == 1);
+            //var singlePerson = people.Single(p => p.Id == 3);
+            //singleZeroElement.PrintItem();
+
+            //.Average()
+            //people.Average(p => p.Age).PrintItem();
+
+            //.Min() .Max()
+            //people.Min(p => p.Age).PrintItem();
+            //people.Max(p => p.Age).PrintItem();
 
         }
     }
