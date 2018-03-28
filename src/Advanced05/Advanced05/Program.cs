6658ed9e7651298938dddb067d5e553ec300229a
Dzevat Ibraimi <Dzevat.Ibraimi@seavus.com>
2018-03-28 17:46:10 +0200

    added Advanced05 solution

---
+++ src/Advanced05/Advanced05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced05
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hi");
+        }
+    }
+}
