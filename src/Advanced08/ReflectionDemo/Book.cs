548a0e44a3f948ff726f163b12937a0d82a5eaee
Dzevat Ibraimi <Dzevat.Ibraimi@seavus.com>
2018-04-04 18:44:34 +0200

    added ReflectionDemo

---
+++ src/Advanced08/ReflectionDemo/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+        public string ToJson()
+        {
+            var result = "";
+            //{ 
+            //"string":"str",
+            //"number":asd
+            //}
+
+            result += "{";
+            result += $"\"id\":{Id},";
+            result += $"\"name\":\"{Name}\",";
+            result += $"\"author\":\"{Author}\"";
+            result += "}";
+            return result;
+        }
+    }
+}
